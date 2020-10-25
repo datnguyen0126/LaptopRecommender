871ed7f70bcd4693a9a22478e56e1b97e06fa012
nguyendat0165 <nguyenhuudat0165@gmail.com>
2020-10-25 23:32:36 +0700

    add question feature

--- LaptopRecommender/api_questions/views.py
+++ LaptopRecommender/api_questions/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from api_questions.models import Questions, Answers
+from api_questions.serializers import AnswerSerializers, QuestionSerializers
+
+
+class QuestionViewSet(viewsets.GenericViewSet):
+    queryset = Questions.objects.select_related('answers')
+    serializer_class = QuestionSerializers
+    permission_classes = [AllowAny, ]
+
+    def list(self, request):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        question = Questions.objects.filter(id=kwargs.get('pk')).first()
+        question_serializer = self.get_serializer(question)
+        return Response(question_serializer.data)
+
+
+    def create(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+    def destroy(self, request):
+        pass
+
